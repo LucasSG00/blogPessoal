2189d4b51532747faaa21c4a43a66c59873bacf2
LucasSG00 <lucas_s_goncalves@hotmail.com>
2021-01-22 12:58:42 -0300

    Criação do Service e config dos End Points logar e cadastrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'entrar', pathMatch:'full'},
+  {path:'inicio', component: InicioComponent},
   {path:'entrar', component: EntrarComponent},
   {path:'cadastrar', component: CadastrarComponent}
 ];

32f886b5e1c6a82aa7923ea3f979ef0e4ba94ccb
LucasSG00 <lucas_s_goncalves@hotmail.com>
2021-01-26 10:58:49 -0300

    Criação da página Tema e sua estilização

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'entrar', pathMatch:'full'},
   {path:'inicio', component: InicioComponent},
   {path:'entrar', component: EntrarComponent},
-  {path:'cadastrar', component: CadastrarComponent}
+  {path:'cadastrar', component: CadastrarComponent},
+  {path:'tema', component: TemaComponent}
 ];
 
 @NgModule({
